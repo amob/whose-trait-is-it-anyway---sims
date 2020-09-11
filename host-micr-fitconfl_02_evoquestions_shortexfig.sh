25e40b1724ec5df5f8f883146f5f20cc5c8a4415
amob <amobrien@ucdavis.edu>
2020-09-11 10:47:59 -0400

    first pass revision

--- host-micr-fitconfl_02_evoquestions_shortexfig.sh
+++ host-micr-fitconfl_02_evoquestions_shortexfig.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --time=0:30:00
-#SBATCH --job-name=simdemofigs
-#SBATCH --nodes=1
+#SBATCH --time=01:40:00
+#SBATCH --job-name=simdemofigs2
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=40
 #SBATCH --output=outandlog/%x_%j.txt
 #SBATCH --mail-type=END
 #SBATCH --mail-user=anna.obrien@utoronto.ca
-#SBATCH --partition=debug
 
 
+module load NiaEnv/2018a # allows loading the correct version of R on the computer on which this was run
 module load r/3.4.3-anaconda5.1.0
 R CMD BATCH --no-restore --no-save $HOME/whosetrait/host-micr-fitconfl_02_evoquestions_shortexfig.R
 
